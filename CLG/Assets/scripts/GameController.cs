5038bc64f6cc2943622e913a41a4315005680c2a
takeo1116 <takeo1116@gmail.com>
2018-08-24 19:35:02 +0900

    Script:GameControllerの変更

---
+++ CLG/Assets/scripts/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+	public ui_control uc;
+	// Use this for initialization
+	void Start () {
+		uc=GameObject.Find("mainField").GetComponent<ui_control>();	//mainFieldのui_controlを読み込む
+		int H=UnityEngine.Random.Range(10,15);	//フィールドの縦を決める[10,15]
+		int W=UnityEngine.Random.Range(10,15);	//フィールドの横を決める[10,15]
+		Debug.Log("(H,W)=("+H+","+W+")");	//デバッグログ
+		int[,] field=new int[H,W];	//盤面を宣言
+		uc.set_panel(H,W,field,0,0,0);	//set_panelを呼び出して盤面を初期化
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
